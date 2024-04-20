f23a0203fd99155d0b2bfe03bc0b5c050bce842d
Taiki Yamashita <toyamash@uci.edu>
2024-04-20 05:10:46 -0700

    data grabbed

--- process_data.py
+++ process_data.py
@@ -1,17 +1,6 @@
 from datasets import load_dataset
-from transformers import AutoTokenizer, AutoModelForCausalLM
-import torch
-from peft import get_peft_model, LoraConfig, TaskType
-from transformers import DataCollatorForLanguageModeling 
-from transformers import Trainer
-import os
+from transformers import AutoTokenizer
 
-device = "cpu" #"xpu" if torch.xpu.is_available() else "cpu"
-model_name = "microsoft/phi-1_5"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-tokenizer.pad_token = tokenizer.eos_token
-
-model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
 class ProcessData:
     def __init__(self):
         self._dataset = None
@@ -22,7 +11,7 @@ class ProcessData:
 
     def format_dataset(self, sample):
         vehicle_title = f"### Vehicle Title\n{sample['Vehicle_Title']}"
-        review = f"### Review\n{sample['Review']}" 
+        review = f"### Review\n{sample['Review']}"
         rating = f"### Rating\n{sample['Rating']}"
         prompt = "\n\n".join([i for i in [vehicle_title, review, rating] if i is not None])
         sample["text"] = f"{prompt}{self.tokenizer.eos_token}"
@@ -32,45 +21,17 @@ class ProcessData:
         return self.tokenizer(examples["text"], padding="max_length", truncation=True, max_length=512)
 
     def run(self):
+        vehicle_list = []
         self._dataset = self._dataset.map(self.format_dataset)
-        tokenized_dataset = self._dataset.map(self.tokenize_function, batched=True, remove_columns=self._dataset.column_names)
-        # Split the dataset into train and test subsets
-        train_test_split = tokenized_dataset.train_test_split(test_size=0.1, seed=42)
-        self._train_dataset = train_test_split["train"]
-        self._test_dataset = train_test_split["test"]
-        
-    def create_model(self):
-        config = LoraConfig(
-            r=16,
-            lora_alpha=2,
-            target_modules=["fc1", "fc2","Wqkv", "out_proj"],
-            lora_dropout=0.05,
-            bias="none",
-            task_type="CAUSAL_LM"
-        )
-
-        self._model = get_peft_model(model, config)
-        self._model.print_trainable_parameters()
-
-    def train(self):
-        os.environ["TOKENIZERS_PARALLELISM"] = "0"  # prevent warnings from training on process forking
-        trainer = Trainer(
-            model=self.model,
-            train_dataset=self._train_dataset,
-            eval_dataset=self._test_dataset,
-            data_collator=DataCollatorForLanguageModeling(tokenizer, mlm=False)
-        )
-
-        if torch.xpu.is_available():
-            torch.xpu.empty_cache()
-        results = trainer.train()
-
-        trainer.save_model("./fine_tuned_model")
-
+        # for i in range(self._dataset.num_rows):
+        for i in range(5):
+            info_dict = {}
+            info_dict[self._dataset["Vehicle_Title"][i]] = self._dataset["Review"][i]
+            vehicle_list.append(info_dict)
+        print(vehicle_list)
 
 if __name__ == "__main__":
     process_data = ProcessData()
     process_data.processed_data()
     process_data.run()
-    process_data.create_model()
-    process_data.train()
+
