2406be403c748d1563536d5a0da789302a303d81
Taiki Yamashita <toyamash@uci.edu>
2024-04-20 01:48:57 -0700

    more done

--- process_data.py
+++ process_data.py
@@ -1,27 +1,36 @@
 from datasets import load_dataset
-from transformers import pipeline
+from transformers import pipeline, AutoTokenizer
 import torch
 
 class ProcessData:
+    def __init__(self):
+        self._dataset = None
+        self.tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+
     def processed_data(self):
-        dataset = load_dataset("florentgbelidji/car-reviews", split='train')
+        self._dataset = load_dataset("florentgbelidji/car-reviews", split='train')
 
     def format_dataset(self, sample):
-        instruction = f"### Instruction\n{sample['instruction']}"
-        context = f"### Context\n{sample['context']}" if len(sample["context"]) > 0 else None
-        response = f"### Answer\n{sample['response']}"
-        prompt = "\n\n".join([i for i in [instruction, context, response] if i is not None])
-        sample["text"] = f"{prompt}{tokenizer.eos_token}"
+        vehicle_title = f"### Vehicle Title\n{sample['Vehicle_Title']}"
+        review = f"### Review\n{sample['Review']}" 
+        rating = f"### Rating\n{sample['Rating']}"
+        prompt = "\n\n".join([i for i in [vehicle_title, review, rating] if i is not None])
+        sample["Author_Name"] = f"{prompt}{self.tokenizer.eos_token}"
 
-    def tokenize_function(examples):
-        return tokenizer(examples["text"], padding="max_length", truncation=True, max_length=512)
+    def tokenize_function(self, examples):
+        return self.tokenizer(examples["Author_Name"], padding="max_length", truncation=True, max_length=512)
+
+    def run(self):
+        self._dataset = self._dataset.map(self.format_dataset)
+        tokenized_dataset = self._dataset.map(self.tokenize_function, batched=True, remove_columns=self._dataset.column_names)
+        # Split the dataset into train and test subsets
+        train_test_split = tokenized_dataset.train_test_split(test_size=0.1, seed=42)
+        train_dataset = train_test_split["train"]
+        test_dataset = train_test_split["test"]
+        print(train_dataset)
+        print(test_dataset)
 
 if __name__ == "__main__":
-    processed_data()
-    dataset = dataset.map(format_dataset)
-    tokenized_dataset = dataset.map(tokenize_function, batched=True, remove_columns=dataset.column_names)
-    # Split the dataset into train and test subsets
-    train_test_split = tokenized_dataset.train_test_split(test_size=0.1, seed=42)
-    train_dataset = train_test_split["train"]
-    test_dataset = train_test_split["test"]
-
+    process_data = ProcessData()
+    process_data.processed_data()
+    process_data.run()
