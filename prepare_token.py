804f7c5dda4ec050b6793f4d05aaba3b2d8eb2f0
Taiki Yamashita <toyamash@uci.edu>
2024-04-19 23:37:25 -0700

    prepare_token setup

---
+++ prepare_token.py
@@ -0,0 +1,32 @@
+import os
+import json
+
+# FREE access token for usage at: tinyurl.com/pg-intel-hack
+import predictionguard as pg
+from getpass import getpass
+
+
+## our access token 'q1VuOjnffJ3NO2oFN8Q9m8vghYc84ld13jaqdF7E'
+pg_access_token = getpass('Enter your Prediction Guard access token: ')
+os.environ['PREDICTIONGUARD_TOKEN'] = pg_access_token
+
+
+messages = [
+{
+"role": "system",
+"content": """You are a Question answer bot and will give an Answer based on the question you get.
+It is critical to limit your answers to the question and dont print anything else.
+If you cannot answer the question, respond with 'Sorry, I dont know.'"""
+},
+{
+"role": "user",
+"content": "I am going to meet my friend for a night out on the town."
+}
+]
+
+result = pg.Chat.create(
+    model="Neural-Chat-7B",
+    messages=messages
+)
+
+print(result['choices'][0]['message']['content'].split('\n')[0])
